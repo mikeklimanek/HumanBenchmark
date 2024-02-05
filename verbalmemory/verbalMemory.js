a8d90e0899a7bc4700b8126483a89fbe5a73d445
mikeklimanek <mike.klimanek@gmail.com>
2024-02-05 03:19:48 +0100

    fully working bot that add words to set and checks if it has seen them already

--- verbalmemory/verbalMemory.js
+++ verbalmemory/verbalMemory.js
@@ -23,18 +23,18 @@ async function main(){
     console.log('Start button clicked');
 
     const seenWords = new Set();
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 500; i++) {       // change the number of iterations to the number of words you want  ( f.e. i < 20; for 20 words)
         async function checkWord(elementHandle) {
             const word = await elementHandle.evaluate(el => el.textContent);
             const [newButton] = await page.$x("//button[contains(@class, 'css-de05nr') and contains(@class, 'e19owgy710') and contains(text(), 'NEW')]");
             const [seenButton] = await page.$x("//button[contains(@class, 'css-de05nr') and contains(@class, 'e19owgy710') and contains(text(), 'SEEN')]");
     
             if (seenWords.has(word)) {
-                console.log('Word already seen:', word);
+                console.log('seen:', word);
                 await seenButton.click();
             } else {
                 seenWords.add(word);
-                console.log('Word added to set:', word);
+                console.log('**Word added to set:', word);
                 await newButton.click();
             }
         }
@@ -45,6 +45,7 @@ async function main(){
         } else {
             console.log('No word element found.');
         }
+        // await page.waitForTimeout(1000);
         
     }
 
