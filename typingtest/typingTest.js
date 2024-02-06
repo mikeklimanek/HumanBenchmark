eddafb33b27cf149452d4b17e40af81f76e1286e
mikeklimanek <mike.klimanek@gmail.com>
2024-02-06 23:28:56 +0100

    typing test initializing

---
+++ typingtest/typingTest.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function main(){
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://humanbenchmark.com/tests/typing', {waitUntil: 'networkidle2'});
+
+    try {     /* clicks 'AGREE' on cookies first to get rid of the pop up */
+        const selector = 'button.css-47sehv, span.css-47sehv';
+        const agreeButton = await page.waitForSelector(selector, { timeout: 5000 });
+        if (agreeButton) {
+          console.log('Accepted cookies');
+          await agreeButton.click();
+        }
+      } catch (error) {
+        console.log('Cookies AGREE button not found:', error);
+      }
+
+}
+
+main();
