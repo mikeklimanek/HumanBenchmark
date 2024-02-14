6261636ebfa3cc7fa2b7804fe636f22eb82f35d9
mikeklimanek <mike.klimanek@gmail.com>
2024-02-14 03:49:13 +0100

    sync all bot installations into one main directory

---
+++ runall.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+const botDirectories = ['./Bot1Directory', './Bot2Directory']; 
+
+async function setupAndStartBot(botDir) {
+  if (!fs.existsSync(`${botDir}/package.json`) || !fs.existsSync(`${botDir}/node_modules`)) {
+    console.log(`Installing dependencies for bot in ${botDir}...`);
+    execSync('npm i puppeteer express dotenv', { cwd: botDir, stdio: 'inherit' });
+  }
+
+  console.log(`Starting bot in ${botDir}...`);
+  execSync('npm start', { cwd: botDir, stdio: 'inherit' });
+}
+
+async function startAllBots() {
+  for (const botDir of botDirectories) {
+    await setupAndStartBot(botDir);
+  }
+}
+
+startAllBots().then(() => console.log('All bots have been started'));
